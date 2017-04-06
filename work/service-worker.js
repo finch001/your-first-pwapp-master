eab03d956741768575e43ae91ad8f0e26c5d8b79
finch <422465778@qq.com>
2017-04-06 21:02:08 +0800

    Initial commit

---
+++ work/service-worker.js
@@ -0,0 +1,58 @@
+/**
+ * Created by finch on 17/4/6.
+ */
+var cacheName = "weatherPWA-step";
+
+var filesToCache = [
+	'/',
+	'/index.html',
+	'/scripts/app.js',
+	'/styles/inline.css',
+	'/images/clear.png',
+	'/images/cloudy-scattered-showers.png',
+	'/images/cloudy.png',
+	'/images/fog.png',
+	'/images/ic_add_white_24px.svg',
+	'/images/ic_refresh_white_24px.svg',
+	'/images/partly-cloudy.png',
+	'/images/rain.png',
+	'/images/scattered-showers.png',
+	'/images/sleet.png',
+	'/images/snow.png',
+	'/images/thunderstorm.png',
+	'/images/wind.png'
+];
+
+self.addEventListener('install', function (event) {
+	console.log("[ServiceWorker] Install");
+	event.waitUntil(
+		caches.open(cacheName).then(function (cache) {
+			console.log("[ServiceWorker] Cacheing app shell");
+			return cache.addAll(filesToCache);
+		})
+	);
+});
+
+self.addEventListener('activate', function (event) {
+	console.log('[ServiceWorker] Activate');
+	event.waitUntil(
+		caches.keys().then(function (keyList) {
+			return Promise.all(keyList.map(function (key) {
+				if (key !== cacheName) {
+					console.log('[ServiceWorker] Removing old cache', key);
+					return caches.delete(key);
+				}
+			}));
+		})
+	);
+	return self.clients.claim();
+});
+
+self.addEventListener('fetch', function (event) {
+	console.log('[ServiceWorker] Fetch', event.request.url);
+	event.respondWith(
+		caches.match(evevt.request).then(function (response) {
+			return response || fetch(e.request);
+		})
+	);
+});
